1d763375b827ca2b2e4ccaf111c9c5ece8e5df60
ksabry97 <isabryex@gmail.com>
2023-06-17 18:57:17 -0700

    asas

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch + '0');
+	}
+	putchar('\n');
+	return (0);
+}
