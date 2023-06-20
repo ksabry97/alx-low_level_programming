476ac4084a30d9650165140b1d20509dba17b33d
ksabry97 <isabryex@gmail.com>
2023-06-20 15:07:21 -0700

    asads

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  print_alphabet - Entry point
+ *
+ * Return: void
+ */
+void  print_alphabet(void)
+{
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
