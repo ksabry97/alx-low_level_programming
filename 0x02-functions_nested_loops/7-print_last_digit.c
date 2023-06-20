cfa2f755b2a56e9e83e2412cd7aa102925d2a6a7
ksabry97 <isabryex@gmail.com>
2023-06-20 16:28:18 -0700

    asass

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *@c: integer
+ * Return: (1) success else (0)
+ */
+int print_last_digit(int c)
+{
+	int x;
+
+	x = c % 10;
+	_putchar(x);
+	return (0);
+}
