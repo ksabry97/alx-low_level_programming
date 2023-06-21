82e21d8fe385e585631752a5f0e696cdc5435b86
ksabry97 <isabryex@gmail.com>
2023-06-20 17:57:39 -0700

    awewaw

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - return the time
+ * Return: void
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int x;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			x = a * b;
+			_putchar(x + '0');
+			if (a != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
