1b9e94e708954a521ad62dab096fc9324c5a2ced
ksabry97 <isabryex@gmail.com>
2023-06-20 17:25:40 -0700

    asss

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,9 +14,9 @@ void jack_bauer(void)
 	{
 		for (b = 0; b < 4; b++)
 		{
-			for (c = 0; c < 5; c++)
+			for (c = 0; c < 6; c++)
 			{
-				for (d = 0; d < 9; d++)
+				for (d = 0; d < 10; d++)
 				{
 					_putchar(a + '0');
 					_putchar(b + '0');
