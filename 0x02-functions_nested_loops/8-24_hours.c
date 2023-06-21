3bac2dbd8098c9500831464c5f6d2bd6a39eef8e
ksabry97 <isabryex@gmail.com>
2023-06-20 17:23:51 -0700

    asas

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - return the time
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0; a < 3; a++)
+	{
+		for (b = 0; b < 4; b++)
+		{
+			for (c = 0; c < 5; c++)
+			{
+				for (d = 0; d < 9; d++)
+				{
+					_putchar(a + '0');
+					_putchar(b + '0');
+					_putchar(':');
+					_putchar(c + '0');
+					_putchar(d + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
