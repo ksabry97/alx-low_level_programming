6d79d3bca2bc84edb93ae11135d8e3fc25c28af4
ksabry97 <isabryex@gmail.com>
2023-06-22 16:37:16 -0700

    aswew

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check uppercase
+ *@c: integer
+ * Return: (1) success else (0)
+ */
+int _isupper(int c)
+{
+	if (c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
