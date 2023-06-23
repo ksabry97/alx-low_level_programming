55f3f88fe15b68e0b4a0ae53b34a90f39ae4ed50
ksabry97 <isabryex@gmail.com>
2023-06-22 19:20:43 -0700

    wdwdw

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_square - print squares
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x = 0;
+
+		while (x < size)
+		{
+			int i = 0;
+
+			while (i < size)
+			{
+				_putchar('#');
+				i++;
+			}
+			_putchar('\n');
+			x++;
+		}
+	}
+}
