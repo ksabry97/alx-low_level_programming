7349de88359b1669d7a58207c9851df490e37501
ksabry97 <isabryex@gmail.com>
2023-06-29 08:33:15 -0700

    copy file

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include <string.h>
+/**
+ * _strncpy - copies a string
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ *
+ * Return: character
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int x;
+	int y;
+
+	x = strlen(dest);
+	y = strlen(src);
+	for (i = 0; n < y ? i < n : i < y; i++)
+	{
+		dest[i] = src[i];
+	}
+}
