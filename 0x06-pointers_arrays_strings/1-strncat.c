677799d763d29a41f37b5da364cdd8e899b2fd23
ksabry97 <isabryex@gmail.com>
2023-06-29 08:23:47 -0700

    asdwdwd

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,8 +11,10 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int x;
+	int y;
 
 	x = strlen(dest);
+	y = strlen(src);
 	for (i = 0; n < y ? i < n : i < y; i++)
 	{
 		dest[x + i] = src[i];
