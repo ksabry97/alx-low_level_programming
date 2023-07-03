a3b25faaaac58730a5da2ee801c639a8698f8118
ksabry97 <isabryex@gmail.com>
2023-07-03 12:11:19 -0700

    adwdasd

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy- makes a new array
+ *@dest : pointer
+ *@src :pointer to character
+ *@n : unsigned integer
+ *
+ * Return: character
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
