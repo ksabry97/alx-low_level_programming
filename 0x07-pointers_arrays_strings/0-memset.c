e52578a829fe042ec5d2555ac462870a8930ffba
ksabry97 <isabryex@gmail.com>
2023-07-03 11:16:38 -0700

    awdw

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - makes encrypted message
+ *@s :pointer
+ *@b :charcater
+ *@n :integer
+ *
+ * Return: character
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
