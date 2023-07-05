e3a8de1da0d0308a69efaf883e44ba65580a4a44
ksabry97 <isabryex@gmail.com>
2023-07-05 04:28:18 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/**
+ * factorial - prints factorial of anumber
+ * @n : integer
+ * Return: integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		n *factorial(n - 1);
+	}
+	return (n);
+}
