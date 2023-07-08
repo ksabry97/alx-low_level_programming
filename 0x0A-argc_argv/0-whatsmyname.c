b2605dbe730ed4e52712bbb5d6234697e4340a46
ksabry97 <isabryex@gmail.com>
2023-07-07 17:53:48 -0700

    awdwd

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+ * main -  return the name of the program
+ * @argc : integer
+ * @argv : array of string
+ *
+ * Return: always (0)
+ **/
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
