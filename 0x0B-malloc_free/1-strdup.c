ea49d3a8fe296ff53160f7c62b2816d9af5ef577
ksabry <isabryex@gmail.com>
2023-07-11 11:28:05 -0700

    asdwdw

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - prints a given string
+ * @str: pointer to string
+ *
+ * Return: character.
+ */
+char *_strdup(char *str)
+{
+	int x = strlen(str);
+	int i = 0;
+	char *y;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	y = malloc(x * sizeof(char));
+
+	if (!y)
+	{
+		return (NULL);
+	}
+	while (i < x)
+	{
+		y[i] = str[i];
+		i++;
+	}
+	return (y);
