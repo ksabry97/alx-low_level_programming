96bf024d9af7d62bd1c9fd7df5dfd8c651e9d00c
ksabry <isabryex@gmail.com>
2023-07-19 06:41:48 -0700

    asdwd

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - prints elements of array of given size
+ * @array: array
+ * @size: size of the array
+ * @action : pointer to afunction
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && size && action)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
