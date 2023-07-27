507aa48f3fd5fee71ea4a2319916775ceba0070f
ksabry <isabryex@gmail.com>
2023-07-27 15:35:19 -0700

    prints linkedlist elments

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *print_list - prints elements of alinked list
+ *@h: pointer to a structure
+ *
+ *Return: the number of elments in the linked list
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n")
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+		count++;
+		return (count);
+}
