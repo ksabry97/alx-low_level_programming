078c6cd50477c66e518baa6716293ce7ec66bb55
ksabry <isabryex@gmail.com>
2023-07-27 15:58:24 -0700

    wddwdw

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *list_len - prints number of elements of alinked list
+ *@h: pointer to a structure
+ *
+ *Return: the number of elments in the linked list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
