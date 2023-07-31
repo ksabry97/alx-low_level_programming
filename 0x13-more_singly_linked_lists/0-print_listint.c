3c1bb076ba3ab07f539d6bf7461fbaf8f614f0ec
ksabry <isabryex@gmail.com>
2023-07-31 11:12:57 -0700

    sdwd

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *print_listint - prints elements of alinked list
+ *@h: pointer to a structure
+ *
+ *Return: the number of elments in the linked list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n",h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
