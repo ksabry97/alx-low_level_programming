7e00e671320cc0b3e34b4b300b5791a248f8335c
ksabry <isabryex@gmail.com>
2023-07-31 11:18:08 -0700

    length of linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *listint_len - prints number of elements of alinked list
+ *@h: pointer to a structure
+ *
+ *Return: the number of elments in the linked list
+ */
+size_t listint_len(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
