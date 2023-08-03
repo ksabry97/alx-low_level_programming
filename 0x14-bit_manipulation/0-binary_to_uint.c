0a3dc030ab9d990c86fe2ab7050a59b889757a5d
ksabry <isabryex@gmail.com>
2023-08-03 15:16:38 -0700

    adwdwd

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to integer
+ * @b: pointer.
+ *
+ * Return: the decimal number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bin;
+	int n = strlen(b);
+	int i;
+	int count = 0;
+
+	bin = 0;
+	for (i = (n - 1); i >= 0; i--)
+	{
+		int c = b[i];
+
+		if (!(c == 48 || c == 49))
+		{
+			return (0);
+		}
+		if (c == 49)
+		{
+			bin = bin + pow(2, count);
+		}
+		count++;
+	}
+	return (bin);
+}
